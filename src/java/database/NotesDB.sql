deb8dc21df806e21071ddfa8f44951241dc442cb
742227 <742227@ELF6IT215805.ACDM.DS.SAIT.CA>
2017-10-30 13:19:05 -0600

    Lab7 doing NoteDB.java file Java Application for lab7.

--- src/java/database/NotesDB.sql
+++ src/java/database/NotesDB.sql
@@ -4,16 +4,14 @@ CREATE DATABASE NotesDB;
 USE NotesDB;
 
 
-DROP TABLE User;
+DROP TABLE Notes;
 
-CREATE TABLE User( 
-    username VARCHAR(10) NOT NULL,
-    password VARCHAR(10) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    active BIT NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    PRIMARY KEY (username)
+CREATE TABLE Notes( 
+    noteID INT NOT NULL AUTO_INCREMENT,
+    dateCreated DATETIME NOT NULL,
+    contents NVARCHAR(1000) CHARACTER SET utf8 NOT NULL,
+  
+    PRIMARY KEY (noteID)
 );
 
 INSERT INTO User values('admin', 'password', 'test@test.com', 1, 'Bob', 'Bobberson');
---
+++ src/java/domainmodel/Note.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domainmodel;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author 742227
+ */
+public class Note implements Serializable{
+    
+
+    private int noteID;
+    private Date dateCreated;
+    private String contents;
+    
+
+    public Note() {
+
+    }
+
+    public int getNoteID() {
+        return noteID;
+    }
+
+    public void setNoteID(int noteID) {
+        this.noteID = noteID;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
