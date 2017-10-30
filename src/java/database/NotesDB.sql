ab7739c7ffdd4f75d7bf39cb4a18f109d78b4850
742227 <742227@ELF6IT215805.ACDM.DS.SAIT.CA>
2017-10-30 11:25:37 -0600

    Lab7 Java Application for lab7.

---
+++ src/java/database/NotesDB.sql
@@ -0,0 +1,19 @@
+DROP DATABASE if exists NotesDB;
+CREATE DATABASE NotesDB;
+
+USE NotesDB;
+
+
+DROP TABLE User;
+
+CREATE TABLE User( 
+    username VARCHAR(10) NOT NULL,
+    password VARCHAR(10) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    active BIT NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+INSERT INTO User values('admin', 'password', 'test@test.com', 1, 'Bob', 'Bobberson');
